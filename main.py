2a23d11fd00e811d2c76ccb29d1f425193539f33
bartu.oeztuerk <bartu.oeztuerk@bonial.com>
2024-06-07 19:48:38 +0200

    netzwerkdiagramm

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from scripts.load_data import load_osm_data
 from scripts.floyd_warshall import floyd_warshall
 from scripts.visualize import plot_distance_matrix
 import networkx as nx
-
+import matplotlib.pyplot as plt
 
 def convert_graph_to_edge_list(graph):
     edge_list = []
@@ -25,10 +25,20 @@ def convert_graph_to_edge_list(graph):
     num_vertices = next_index  # Anzahl der einzigartigen Knoten
     return edge_list, num_vertices
 
+def draw_network(graph):
+    print("Zeichne das Netzwerkdiagramm...")
+    pos = nx.spring_layout(graph)  # Positionen der Knoten im 2D-Raum
+    nx.draw_networkx_nodes(graph, pos, node_size=10, node_color='blue', alpha=0.6)
+    nx.draw_networkx_edges(graph, pos, alpha=0.4)
+    plt.title('Netzwerkdiagramm')
+    plt.show()
 
 def main():
     print("Laden des Graphen...")
     graph = load_osm_data()
+
+    print("Zeichne das Netzwerkdiagramm...")
+    draw_network(graph)
 
     print("Konvertiere Graph zu Edge List...")
     edge_list, num_vertices = convert_graph_to_edge_list(graph)
@@ -42,7 +52,6 @@ def main():
     print("Visualisiere die Distanzmatrix...")
     plot_distance_matrix(distances)
     print("Visualisierung abgeschlossen.")
-
 
 if __name__ == "__main__":
     main()
